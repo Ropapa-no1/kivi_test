151c4d9f2a238fdba9852ee0e744535cbaf71cd1
Ropapa <roland.toth@mediso.com>
2023-11-16 10:10:06 +0100

    egy

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print ("e") # sdfsdfsd
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
 
+class MyApp(App):
+    def build(self):
+        return Label(text="Hej")
+    
+
+if __name__ == "__main__":
+    MyApp().run()
